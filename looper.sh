8335fc92514c6f177f534930d4ee9249b1544971
Chatur <jasonbright2709@gmail.com>
2021-05-05 23:04:06 +0400

    Update looper.sh

--- looper.sh
+++ looper.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
 COUNTER=0
-         while [  $COUNTER -lt 100000 ]; do
+         while [  $COUNTER -lt 20000 ]; do
              echo $(( RANDOM - ( RANDOM % RANDOM ) )) > looper.txt
 	     git add looper.txt
              git commit -m "Commits: Loop at $(date -u +%D-%T%P)"
 	     let COUNTER=COUNTER+1
-	     if ["${COUNTER}" == 20000];
-	     then
-		git push origin master
-	     elif ["${COUNTER}" == 40000];
-	     then
-		git push origin master
-	     elif ["${COUNTER}" == 60000];
-	     then
-		git push origin master
-	     elif ["${COUNTER}" == 80000];
-	     then
-		git push origin master
-	     elif ["${COUNTER}" == 100000];
-	     then
-	     	git push origin master
-	     fi
          done
+git push origin master
 
